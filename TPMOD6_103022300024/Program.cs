6a66792912f451c56ac063b9a249a2fd6ce53fbd
Zipooxy <142865402+Zipooxy@users.noreply.github.com>
2025-03-23 23:48:25 +0700

    hapus pesan

--- TPMOD6_103022300024/Program.cs
+++ TPMOD6_103022300024/Program.cs
@@ -8,7 +8,7 @@ public class SayaTubeVideo
     private string title;
     private int playCount;
 
-    // Constructor dengan Prekondisi
+  
     public SayaTubeVideo(string title)
     {
         Debug.Assert(!string.IsNullOrEmpty(title), "Judul video tidak boleh null atau kosong");
@@ -16,12 +16,12 @@ public class SayaTubeVideo
 
 
         Random random = new Random();
-        this.id = random.Next(10000, 99999); // Generate random 5-digit id
+        this.id = random.Next(10000, 99999); 
         this.title = title;
-        this.playCount = 0; // Initialize playCount to 0
+        this.playCount = 0; 
     }
 
-    // Method untuk menambah play count dengan validasi
+    
     public void IncreasePlayCount(int count)
     {
         Debug.Assert(count > 0 && count <= 10000000, "Jumlah play count harus antara 1 dan 10.000.000");
@@ -39,7 +39,7 @@ public class SayaTubeVideo
         }
     }
 
-    // Method untuk mencetak detail video
+    
     public void PrintVideoDetails()
     {
         Console.WriteLine("==========================");
@@ -56,18 +56,18 @@ class Program
     {
         try
         {
-            // Menguji prekondisi (judul valid dan panjang tidak lebih dari 100 karakter)
+            
             SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – jack kelvin G.R");
             video.PrintVideoDetails();
 
-            // Menguji batas maksimal play count (valid input)
+          
             video.IncreasePlayCount(10000000);
             video.PrintVideoDetails();
 
-            // Menguji exception overflow dengan loop
+           
             for (int i = 0; i < 50; i++)
             {
-                video.IncreasePlayCount(10000000); // Loop untuk memaksa overflow
+                video.IncreasePlayCount(10000000); 
             }
         }
         catch (Exception ex)
